e2d3336c94a43f9895ef2bed1cfeeeb338aa549e
Sarthak Singh <sarthak1912@gmail.com>
2022-06-03 19:04:45 +0530

    Updates

---
+++ precache-manifest.1efe03ce7aa53eef600e55149fd938bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb62fcb851c0067701d805ad3c50838",
+    "url": "/box-office-app-1/index.html"
+  },
+  {
+    "revision": "4ad0786b134e8c49f916",
+    "url": "/box-office-app-1/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "1d7ef641e7956a85dced",
+    "url": "/box-office-app-1/static/js/2.9cd1a699.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office-app-1/static/js/2.9cd1a699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ad0786b134e8c49f916",
+    "url": "/box-office-app-1/static/js/main.77383d24.chunk.js"
+  },
+  {
+    "revision": "fad5f5ffad5ccb32398b",
+    "url": "/box-office-app-1/static/js/runtime-main.02f2f461.js"
+  }
+]);
